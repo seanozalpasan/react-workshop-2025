4e6cad1b8734ceca6dfb5b04d9014ae9a9b1b9c4
sean <seanozalpasan@gmail.com>
2025-10-15 19:33:51 -0400

    added stuff

--- src/components/AddCardModal.jsx
+++ src/components/AddCardModal.jsx
@@ -15,11 +15,15 @@ export default function AddCardModal({ onAddCard }) {
   const handleSubmit = (e) => {
     // Validate input: neither field can be empty. If either is empty, do nothing (return null).
 
-
+    if(question === "" || answer === ""){
+      return null;
+    }
 
     // This function cannot manipulate the cards array directly (think about why!).
     // Instead, it needs to call the onAddCard function passed down from FlashcardPage and
     // provide the new card data as an argument. Call onAddCard with the new card data here:
+
+    //NEED TO DO THIS AND SUBMIT BUTTON
 
     // Clear the input fields after submission
 
